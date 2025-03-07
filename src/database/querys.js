ec8d3c2c981ceacd430e24867a6a427601929832
Carlos José Sierra <97687014+CarlosJoseSierra@users.noreply.github.com>
2025-03-07 17:53:18 -0500

    quince20 commit global

--- src/database/querys.js
+++ src/database/querys.js
@@ -1,6 +1,6 @@
 export const querys = {
   getAllProducts: "SELECT * FROM PRODUCTO  WHERE PROD_estado = 1 ORDER BY PROD_nombre",
-  getAllProducts: "SELECT * FROM PRODUCTO  WHERE PROD_estado = 1 AND PROD_costoUnitarioH = 1 ORDER BY PROD_nombre",
+  getAllProductsVenta: "SELECT * FROM PRODUCTO  WHERE PROD_estado = 1 AND PROD_costoUnitarioH = 1 ORDER BY PROD_nombre",
   getProductById: "SELECT * FROM PRODUCTO Where PROD_id = @Id",
   addNewProduct:
     "INSERT INTO PRODUCTO (PROD_codigo, PROD_nombre, PROD_medida) VALUES (@name,@description,@quantity);",
